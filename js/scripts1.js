beb10f894cf0cb29e21e379b9d11fc9bec379ef7
shivaprasad.swami <shivaprasad.swami@twinspires.com>
2016-08-30 14:46:28 -0700

    added the images

--- js/scripts1.js
+++ js/scripts1.js
@@ -3,13 +3,23 @@
 * Manufacture - Square
 * Regional distribution centers - Triangle
 * Store -  Circle
+* MAY 6 2014 - https://icons8.com/web-app/new-icons/android
+* Media Controls - https://icons8.com/web-app/category/android/Media-Controls
 */
-var serviceCenter = '../icons/hexagon-16.png';
-var manufacture = '../icons/square-16.png';
-var regDistCenter = '../icons/triangle-16.png';
-var store = '../icons/circle-16.png';
+/*var serviceCenter = '../maps/icons/hexagon-16.png';
+var manufacture = '../maps/icons/square-16.png';
+var regDistCenter = '../maps/icons/triangle-16.png';
+var store = '../maps/icons/circle-16.png';
+*/
+var serviceCenter = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAA1klEQVQ4T62S4RGCMAyFX5nAEZqzC7gBTqBs4AY6im7gBrKBOoELwDUjMAH1Cqi0lIKn+dkkX95LI/BjiFi/lnph88RUjdVNAIocgCFW2dcALYstIC5to8mIlYUNIqiglV5rAI0FABWQUMjKCMBKFxt3nMlDVgYALXUK1New52RNTLd+zgF00h8A5MjSGEhWfSseoDwC2E+cxol4eXjVvAFx6T7yY6UHKM4R6T6BidXOPkYPac6VexZg5jR15313FGhZzm5uActmeH8Hzv9OKSFW6V928AQv7EYRIGDRnAAAAABJRU5ErkJggg==';
+var manufacture = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAwAAAAMCAYAAABWdVznAAAASElEQVQoU2NkIBEwgtRX6Nx2YGJgssen9x/Dv4MdV1QPgDVU6dxuYGBgrMdv2f/GtiuqDaMaaBdKkIhjcMAfcQwH4BFHSuoAAKLVPQ0hWb7FAAAAAElFTkSuQmCC';
+var regDistCenter = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAA8klEQVQ4T6WTOw6CQBCGZxSCpTdQC6hBLqAnUE+gNAZPpLEBb4An0Asg1FioN7CU8BizGhLBhaBstcnO/83+80BoeLBKr24Dlb37S8UviysFqNalK4TRgQljSRz7xuDOg5QC9HVgI+KciQjIdk3FqA0YroNFC9H6FKRExmml2EXI1w+YbyFFj5ctbpFWrEcOwHy3w8hDwD4PQEDXRBK1z3rkAPrm7CDApKozBOC4pjzLYv4B7F1TnnIBLwuP2EeEHtcCwS3pCGqpBSZqVMQsa6M2ZpDcIBHt3JWyqD1ILyvvlh7ZPZHE0c+jnNXj72Wqu+VPAnlpEX2G9rUAAAAASUVORK5CYII=';
+//var store = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAwAAAAMCAYAAABWdVznAAAAuklEQVQoU51SsQ3CMBD0pQLJBSOkTyxgA5gEUiYVm5CKlMAkMEKsOH1GoLAE3SEHObKQQAFX9vvu/t5niGAdkmQngA2AhSuTrAVZFm179jC4zTGOZ3cpLx4Yinji1Np11nW3nlClaS2A+TswPLtuhTFLOBuIov03cHCXYYy6J7guqJTiSPUe9gdhxMCDA1L/PvTYZxWkzo1ZDME9pLx+zILUE2tXQ3DeY6XUVpDue7xCJLUAyrxpTh7zBPqjTgsSfSZeAAAAAElFTkSuQmCC';
+var store = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAwAAAAMCAYAAABWdVznAAAAnUlEQVQoU52RsRWCQBBEZ2wJcjGDjAIQS6CFowNL8IG5zwhCyM+WHN+dogY8PLlwb/7uzizx5+O3PsqKnGIFMvF1aQBU2/48TLo3EKXlicRhdqBQ274x7s8DvjM2l8XtdN+5SR6Is/0AcLtsR6Pt2uQFlArxbruGa4HfK0m63vo2X2f6mVR5JFAFxTqJ4rRIQJpPYhohmdnDhaTkNA8+Q0MNP+MHiQAAAABJRU5ErkJggg==';
 var allStates = citiesMap;
 console.log(allStates);
+var allStates1 = states;
+console.log(allStates1);
 
 function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
@@ -24,7 +34,7 @@ function initMap() {
     return longLat;
   }
   function generateRandomPaths() {
-    for(var i = 1; i<100; i++) {
+    for(var i = 1; i<20; i++) {
       var flightPlanCoordinates = [];
       for(var j = 1; j<5; j++) {
         var randomArea = Math.floor(Math.random()*allStates.length);
@@ -79,14 +89,15 @@ function initMap() {
     {lat: 34.9513, lng: -92.3809}, // AL
   ];
   console.log(flightPlanCoordinatesss);
+
   generateRandomPaths();
 
 
   function generateRandomSpots() {
-    for(var i = 0; i<20; i++) {
-      var randomArea = Math.floor(Math.random()*allStates.length);
-      var latitude = allStates[randomArea].latitude;
-      var longitude = allStates[randomArea].longitude;
+    for(var i = 0; i<allStates1.length; i++) {
+      //var randomArea = Math.floor(Math.random()*allStates1.length);
+      var latitude = allStates1[i].latitude;
+      var longitude = allStates1[i].longitude;
       var cordi = {lat: parseFloat(latitude), lng: parseFloat(longitude)}
       drawSpots(cordi);
       }
